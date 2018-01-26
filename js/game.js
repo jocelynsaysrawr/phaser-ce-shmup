f5255d8e46824afa4b749b02a0f679f31f78ded5
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-25 19:03:43 -1000

    added spaceship

--- js/game.js
+++ js/game.js
@@ -3,17 +3,20 @@
   const GAME_WIDTH = 400;
   const GAME_HEIGHT = 400;
   const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+
+  let player;
 
 
   //core game methods
   function preload() {
-
+    game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
   }
 
   function create() {
-
+    player = game.add.sprite(100, 100, GFX, 8);
   }
 
   function update() {
